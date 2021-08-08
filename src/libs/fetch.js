f6f4bbbc7eb9798b75154a461ad13b73ba138184
Io Kobayashi <io.kobayashiii@gmail.com>
2021-08-09 01:06:52 +0900

    fix

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -3,6 +3,6 @@ export function fetchMicroCMS(endpointStrings) {
 	for(let i = 0; i < endpointStrings.length; i++) {
 		i === 0 ? queryString = endpointStrings[0] : queryString += `/${endpointStrings[i]}`
 	}
-	const requestUrl = process.env.BASE_URL + queryString
+	const requestUrl = 'https://for.microcms.io/api/v1/' + queryString
 	return fetch(requestUrl, { headers: { 'X-API-KEY': process.env.API_KEY } })
 }
