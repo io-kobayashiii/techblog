58875c973c2427f6bb76f6a97c62c830a339f991
io-kobayashiii <io.kobayashiii@gmail.com>
2021-09-23 05:07:42 +0900

    #21 - add ArticleCard component

--- src/components/atoms/text/ArticleCardTitle.tsx
+++ src/components/atoms/text/ArticleCardTitle.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import { CheckIfItExistsInStyles } from '../../../libs/CheckIfItExistsInStyles'
 
 type Props = {
 	displayText: string
 	additionalClasses?: string[]
 }
 
-const ArticleCardTitle = ({displayText, additionalClasses}: Props) => {
+const ArticleCardTitle = ({displayText, additionalClasses = ['']}: Props): JSX.Element => {
 	const commonClasses = ['text-white', 'text-16', 'sm:text-18', 'md:text-20', 'lg:text-24', 'font-bold']
 	const classes = [...commonClasses, ...additionalClasses]
 	return (
