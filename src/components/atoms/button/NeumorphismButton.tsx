7da448622ed8a970de8119bb84e6555cf723cac2
io-kobayashiii <io.kobayashiii@gmail.com>
2021-09-26 23:11:52 +0900

    fix

--- src/components/atoms/button/NeumorphismButton.tsx
+++ src/components/atoms/button/NeumorphismButton.tsx
@@ -10,10 +10,10 @@ type Props = {
 
 const NeumorphismButton = ({unevenness, displayText, additionalClasses = ['']}: Props): JSX.Element => {
 	const unevennessClass = unevenness == 'dents' ? styles.dents : styles.bumps
-	const commonClasses = ['text-white', 'py-5', 'px-15', 'md:py-8', 'md:px-12', 'text-12', 'md:text-14']
+	const commonClasses = ['']
 	const classes = [unevennessClass, ...commonClasses, ...CheckIfItExistsInStyles(additionalClasses, styles)]
 	return (
-		<button className={classes.join(' ')}>{displayText}</button>
+		<button className={classes.join(' ')} dangerouslySetInnerHTML={{ __html: displayText }}></button>
 	)
 }
 
