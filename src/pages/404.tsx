118dc1eb9eaaf58bbe0a5b6a5f4bc9ce5b323792
io kobayashi <io.kobayashiii@gmail.com>
2021-10-26 03:24:57 +0900

    add Layout and close #43

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,17 @@
+import { fetchMicroCMS } from '@/libs/fetch'
+
 export default function Custom404() {
-	return <main className="main">Not found.</main>
+	return <h1 className="text-24">Not found.</h1>
+}
+
+export const getStaticProps = async () => {
+	const fetchCategories = await fetchMicroCMS(['categories'])
+	const categoryList = await fetchCategories.json()
+
+	return {
+		props: {
+			layout: '404',
+			categories: categoryList.contents,
+		},
+	}
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import '@/styles/tailwind.css'
 import '@/styles/globals.css'
+import DefaultLayout from '@/layouts/DefaultLayout'
 
 function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	// 現状出し分けるレイアウトがないが今後のために実装
+	switch (pageProps.layout) {
+		case 'default':
+			return (
+				<DefaultLayout categories={pageProps.categories}>
+					<Component {...pageProps} />
+				</DefaultLayout>
+			)
+		case '404':
+			return (
+				<DefaultLayout categories={pageProps.categories}>
+					<Component {...pageProps} />
+				</DefaultLayout>
+			)
+	}
 }
 
 export default App
