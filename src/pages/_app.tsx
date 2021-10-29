47837e272d13c5680a4d87f9512da32a481e862b
io kobayashi <io.kobayashiii@gmail.com>
2021-10-30 00:15:31 +0900

    #50 - add tailwind classes used in the top page to app.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import DefaultLayout from '@/layouts/DefaultLayout'
 import ArticleLayout from '@/layouts/ArticleLayout'
 
 function App({ Component, pageProps }) {
+	pageProps['tailwindClasses'] =
+		'pt-70 md:pt-180 pb-50 md:pb-80 bg-gray-100 px-15 md:px-30 max-w-lg mx-auto'
 	switch (pageProps.layout) {
 		case 'default':
 			return (
