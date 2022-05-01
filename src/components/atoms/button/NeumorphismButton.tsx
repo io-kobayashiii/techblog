e2d32a7ea4b2f8efc8d6beb414aef720670556bd
io-kobayashiii <io.kobayashiii@gmail.com>
2022-05-02 00:02:06 +0900

    fixed to use useContext

--- src/components/atoms/button/NeumorphismButton.tsx
+++ src/components/atoms/button/NeumorphismButton.tsx
@@ -11,13 +11,13 @@ const NeumorphismButton = ({ unevenness, shadowColor, displayText, className }: 
 	const getStyle = () => {
 		switch (true) {
 			case unevenness == 'dents' && shadowColor == 'default':
-				return { 'box-shadow': '2px 2px 2px 0 rgba(0, 0, 0, 0.2) inset, -2px -2px 2px 0 rgba(255, 255, 255, 0.5) inset' } as React.CSSProperties
+				return { boxShadow: '2px 2px 2px 0 rgba(0, 0, 0, 0.2) inset, -2px -2px 2px 0 rgba(255, 255, 255, 0.5) inset' } as React.CSSProperties
 			case unevenness == 'dents' && shadowColor == 'primary':
-				return { 'box-shadow': '2px 2px 2px 0 rgba(6, 13, 19, 0.5) inset, -2px -2px 2px 0 rgba(231, 231, 231, 0.5) inset' } as React.CSSProperties
+				return { boxShadow: '2px 2px 2px 0 rgba(6, 13, 19, 0.5) inset, -2px -2px 2px 0 rgba(231, 231, 231, 0.5) inset' } as React.CSSProperties
 			case unevenness == 'bumps' && shadowColor == 'default':
-				return { 'box-shadow': '2px 2px 2px 0 rgba(0, 0, 0, 0.5)' } as React.CSSProperties
+				return { boxShadow: '2px 2px 2px 0 rgba(0, 0, 0, 0.5)' } as React.CSSProperties
 			case unevenness == 'bumps' && shadowColor == 'primary':
-				return { 'box-shadow': '2px 2px 2px 0 rgba(4, 9, 14, 0.5), -2px -2px 2px 0 rgba(36, 81, 129, 0.5)' } as React.CSSProperties
+				return { boxShadow: '2px 2px 2px 0 rgba(4, 9, 14, 0.5), -2px -2px 2px 0 rgba(36, 81, 129, 0.5)' } as React.CSSProperties
 		}
 	}
 	return <button className={`${className ? className : ''} ${unevenness == 'dents' ? 'cursor-auto pointer-events-none' : ''}`} style={getStyle()} dangerouslySetInnerHTML={{ __html: displayText }}></button>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,6 @@ class CustomDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
-					<link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200&display=swap" rel="stylesheet" />
 					<link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/all.min.css" />
 					<link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200&family=M+PLUS+1p:wght@400&display=swap" rel="stylesheet" />
 				</Head>
