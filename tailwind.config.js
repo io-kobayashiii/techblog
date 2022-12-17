4841e7d7e3a6001ebc2f02d3e227f929ca683cd5
io-kobayashiii <io.iobayashiii@gmail.com>
2022-12-17 17:02:53 +0900

    #90 : Update icon resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,16 +15,78 @@ module.exports = {
           github: '#171515',
         },
       },
-      maxWidth: {
-        sm: '560px',
-        md: '768px',
-        lg: '1000px',
+      scale: {
+        flip: '-1',
+      },
+      minHeight: {
+        '100vh': '100vh',
+      },
+      borderWidth: {
+        3: '3px',
+        5: '5px',
       },
     },
     screens: {
-      sm: '560px',
+      xs: '375px',
+      sm: '600px',
       md: '768px',
-      lg: '1000px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1360px',
+    },
+    maxWidth: {
+      ...[...Array(1201)].reduce((m, _, i) => {
+        m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
+        return m;
+      }, {}),
+      xs: '375px',
+      sm: '600px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1360px',
+      none: 'none',
+    },
+    minWidth: {
+      ...[...Array(1201)].reduce((m, _, i) => {
+        m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
+        return m;
+      }, {}),
+      xs: '375px',
+      sm: '600px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1360px',
+      none: 'none',
+    },
+    maxHeight: {
+      ...[...Array(1201)].reduce((m, _, i) => {
+        m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
+        return m;
+      }, {}),
+    },
+    minHeight: {
+      ...[...Array(1201)].reduce((m, _, i) => {
+        m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
+        return m;
+      }, {}),
     },
     borderRadius: {
       ...[...Array(32)].reduce((m, _, i) => {
@@ -38,10 +100,18 @@ module.exports = {
         m[i] = `${i}px`;
         return m;
       }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}vw`] = `${i}vw`;
+        return m;
+      }, {}),
     },
     padding: {
-      ...[...Array(201)].reduce((m, _, i) => {
+      ...[...Array(1001)].reduce((m, _, i) => {
         m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
         return m;
       }, {}),
       ...[...Array(5)].reduce((m, _, i) => {
@@ -50,11 +120,11 @@ module.exports = {
       }, {}),
     },
     margin: {
-      ...[...Array(201)].reduce((m, _, i) => {
+      ...[...Array(1001)].reduce((m, _, i) => {
         m[i] = `${i}px`;
         return m;
       }, {}),
-      ...[...Array(201)].reduce((m, _, i) => {
+      ...[...Array(1001)].reduce((m, _, i) => {
         m[`minus-${i}`] = `-${i}px`;
         return m;
       }, {}),
@@ -69,6 +139,8 @@ module.exports = {
         m[`${i}p`] = `${i}%`;
         return m;
       }, {}),
+      auto: 'auto',
+      fit: 'fit-content',
     },
     height: {
       ...[...Array(1001)].reduce((m, _, i) => {
@@ -79,12 +151,63 @@ module.exports = {
         m[`${i}p`] = `${i}%`;
         return m;
       }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}vh`] = `${i}vh`;
+        return m;
+      }, {}),
+      fit: 'fit-content',
     },
     lineHeight: {
       ...[...Array(101)].reduce((m, _, i) => {
         m[i] = `${i}px`;
         return m;
       }, {}),
+      '1em': '1em',
+    },
+    zIndex: {
+      ...[...Array(10001)].reduce((m, _, i) => {
+        m[i] = `${i}`;
+        return m;
+      }, {}),
+      'minus-1': '-1',
+      auto: 'auto',
+    },
+    translate: {
+      ...[...Array(1001)].reduce((m, _, i) => {
+        m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(1001)].reduce((m, _, i) => {
+        m[`minus-${i}`] = `-${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`minus-${i}p`] = `-${i}%`;
+        return m;
+      }, {}),
+    },
+    inset: {
+      ...[...Array(1001)].reduce((m, _, i) => {
+        m[i] = `${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(1001)].reduce((m, _, i) => {
+        m[`minus-${i}`] = `-${i}px`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`${i}p`] = `${i}%`;
+        return m;
+      }, {}),
+      ...[...Array(101)].reduce((m, _, i) => {
+        m[`minus-${i}p`] = `-${i}%`;
+        return m;
+      }, {}),
+      auto: 'auto',
     },
   },
   variants: {
