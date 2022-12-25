97bddf7aea79467afa07179269c751c265912721
io-kobayashiii <io.iobayashiii@gmail.com>
2022-12-26 00:10:12 +0900

    Commit test

--- src/components/button/NeumorphismButton.stories.tsx
+++ src/components/button/NeumorphismButton.stories.tsx
@@ -1,26 +1,46 @@
-import React from 'react'
-import NeumorphismButton from './NeumorphismButton'
+import React from 'react';
+import NeumorphismButton from './NeumorphismButton';
 
 const exportComponent = {
-	component: NeumorphismButton,
-	title: 'NeumorphismButton',
-}
-export default exportComponent
+  component: NeumorphismButton,
+  title: 'NeumorphismButton',
+};
+export default exportComponent;
 
-const Template = (args) => <NeumorphismButton {...args} />
+const Template = (args) => <NeumorphismButton {...args} />;
 
-export const DentsDefault = Template.bind({})
+export const DentsDefault = Template.bind({});
 DentsDefault.args = {
-	unevenness: 'dents',
-	shadowColor: 'default',
-	displayText: 'button',
-	className: ['m-5', 'rounded-100vh', 'py-5', 'px-15', 'md:py-8', 'md:px-12', 'text-12', 'md:text-14', 'bg-gray-100'],
-}
+  unevenness: 'dents',
+  shadowColor: 'default',
+  displayText: 'button',
+  className: [
+    'm-5',
+    'rounded-100vh',
+    'py-5',
+    'px-15',
+    'md:py-8',
+    'md:px-12',
+    'text-12',
+    'md:text-14',
+    'bg-gray-100',
+  ],
+};
 
-export const BumpsDefault = Template.bind({})
+export const BumpsDefault = Template.bind({});
 BumpsDefault.args = {
-	unevenness: 'bumps',
-	shadowColor: 'default',
-	displayText: 'button',
-	className: ['m-5', 'rounded-100vh', 'py-5', 'px-15', 'md:py-8', 'md:px-12', 'text-12', 'md:text-14', 'bg-gray-100'],
-}
+  unevenness: 'bumps',
+  shadowColor: 'default',
+  displayText: 'button',
+  className: [
+    'm-5',
+    'rounded-100vh',
+    'py-5',
+    'px-15',
+    'md:py-8',
+    'md:px-12',
+    'text-12',
+    'md:text-14',
+    'bg-gray-100',
+  ],
+};
