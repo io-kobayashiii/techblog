ccc70f0f6185c5b13071d205e52c8ad42fc49088
io-kobayashiii <io.kobayashiii@gmail.com>
2023-02-25 14:26:17 +0900

    Remove stylesheet for categories page

--- src/pages/categories/[slug].tsx
+++ src/pages/categories/[slug].tsx
@@ -1,6 +1,5 @@
 import * as ArticleTypes from '@/types/ArticleTypes';
 import { ArticleCard } from '@/components/Card/ArticleCard';
-import styles from '@/styles/pages/categories/categories.module.scss';
 import 'highlight.js/styles/stackoverflow-dark.css';
 import ApiRequests from '@/utils/ApiClient';
 import { GetStaticProps } from 'next';
@@ -14,7 +13,7 @@ type Props = {
 const CategoriesIndex = ({ articles, slugs }: Props) => {
   return (
     <>
-      <h1 className={`${styles.heading} text-24 sm:text-28 md:text-32`}>
+      <h1 className={`font-bold text-24 sm:text-28 md:text-32`}>
         Category: {slugs}
       </h1>
       <ul className="mt-30 md:mt-50 md:flex md:flex-wrap md:justify-between">
