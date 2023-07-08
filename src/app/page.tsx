78b08b990cb874bc33a81dacb1fdfff07e00aacc
io-kobayashiii <io.kobayashiii@gmail.com>
2023-07-09 04:58:01 +0900

    #163: Add pseudo additional fetched articles component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,33 @@
 import { ArticleCard } from '@/components/Card/ArticleCard';
-import ApiClient from '@/utils/ApiClient';
+import PseudoAdditionalFetchedArticles from '@/components/PseudoAdditionalFetchedArticles/PseudoAdditionalFetchedArticles';
+import { getAllArticles } from '@/utils/microCmsClient';
 import { notFound } from 'next/navigation';
 
 export default async function Page() {
-  const articles = await ApiClient.articles();
+  const articles = await getAllArticles();
   if (!articles) notFound();
   return (
     <>
       <main className="bg-gray-800 pt-76 pb-20 md:pt-180 md:pb-80">
         <div className="mx-auto max-w-lg px-15 md:px-30">
           <ul className="md:flex md:flex-wrap md:justify-between">
-            {articles.contents?.map((article, index) => (
-              <li
-                key={article.id}
-                className={
-                  index == 0
-                    ? 'md:w-100p'
-                    : 'mt-15 md:mt-30 md:w-[calc(50%-15px)]'
-                }
-              >
-                <ArticleCard
-                  unevenness="bumps"
-                  shadowColor="default"
-                  data={{
-                    title: article.title,
-                    date: article.publishedAt,
-                    href: `/articles/${article.id}`,
-                    categories: article.categories.map(
-                      (category) => category.name
-                    ),
-                  }}
-                />
-              </li>
-            ))}
+            {articles
+              .slice(0, 10)
+              .map(({ id, title, publishedAt, categories }) => (
+                <li key={id} className={'mt-15 md:mt-30 md:w-[calc(50%-15px)]'}>
+                  <ArticleCard
+                    unevenness="bumps"
+                    shadowColor="default"
+                    data={{
+                      title,
+                      date: publishedAt,
+                      href: `/articles/${id}`,
+                      categories: categories.map(({ name }) => name),
+                    }}
+                  />
+                </li>
+              ))}
+            <PseudoAdditionalFetchedArticles articles={articles.slice(10)} />
           </ul>
         </div>
       </main>
---
+++ src/components/Card/ArticleCardInLoading/ArticleCardInLoading.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ArticleCard } from '../ArticleCard/ArticleCard';
+
+export default {
+  title: 'ArticleCard',
+  component: ArticleCard,
+} as ComponentMeta<typeof ArticleCard>;
+
+const Template: ComponentStory<typeof ArticleCard> = (args) => (
+  <ArticleCard {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  unevenness: 'bumps',
+  shadowColor: 'default',
+  data: {
+    title: 'title',
+    date: '2021.01.01',
+    href: 'https://nextjs.org/',
+    categories: ['JavaScript', 'HTML', 'CSS', 'SCSS'],
+  },
+};
