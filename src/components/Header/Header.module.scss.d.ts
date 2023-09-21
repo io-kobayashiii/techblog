8d644c9ad8bd0eabe7a479bfb87b302c77c19439
io-kobayashiii <io.kobayashiii@gmail.com>
2023-09-22 00:00:15 +0900

    #182: Replace icons

--- src/components/Header/Header.module.scss.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const border: string;
-export declare const dents: string;
-export declare const header: string;
-export declare const isGlobalNavigationOpen: string;
-export declare const logo: string;
